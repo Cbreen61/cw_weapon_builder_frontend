58c828e85a047952f6ab38273caeaa12a03518f4
Christopher Breen <C.jbreen61@gmail.com>
2021-03-12 11:15:54 -0500

    moved fetch call for weapons into function getWeapons

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const BASE_URL = "http://[::1]:3000/api/v1/weapons"
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    fetch(BASE_URL)
-    .then(resp => console.log(resp))
+    getWeapons();
 
 })
+
+function getWeapons(){
+    fetch(BASE_URL)
+    .then(resp => resp.json())
+    .then(weapons => {
+        console.log(weapons);
+    })
+}
