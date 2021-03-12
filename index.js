74e1151c43d7810336b9ec85c547a8be88e21499
Christopher Breen <C.jbreen61@gmail.com>
2021-03-12 10:55:49 -0500

    add base url for api annd started domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("you in it")
+const BASE_URL = "http://[::1]:3000/api/v1/weapons"
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetch(BASE_URL)
+    .then(resp => console.log(resp))
+
+})
