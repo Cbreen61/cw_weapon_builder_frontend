c7d8b99c9a57d8e84ae01d9c64b04e0d0c371d0d
Christopher Breen <C.jbreen61@gmail.com>
2021-03-12 11:54:40 -0500

    added weapons-form to inde.html, alsot started 2 functions one to handle form rendering the other to handle form submission

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const BASE_URL = "http://[::1]:3000/api/v1/weapons"
 
 document.addEventListener('DOMContentLoaded', ()=>{
     getWeapons();
+    createForm();
 
 })
 
@@ -22,5 +23,39 @@ function getWeapons(){
             document.querySelector('#weapons-container').innerHTML += weaponMarkup
             
         })
+    })
+}
+
+function createForm(){
+    let weaponsForm = document.getElementById("weapons-form")
+
+    weaponsForm.innerHTML +=
+    `
+    <form>
+        <label> Name:</label><input type="text" id= "name"><br>
+        <label> Image:</label><input type="text" id= "image"><br>
+        <input type="submit" value="Add Weapon">
+    </form>
+    `
+    weaponsForm.addEventListener("submit", weaponsFormSubmission)
+}
+function weaponsFormSubmission(){
+    event.preventDefault();
+    let name = document.getElementById("name").value
+    let image = document.getElementById("image").value
+    
+    let weapon = {
+        name: name,
+        image: image
+    }
+
+    fetch(`${BASE_URL}`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(weapon)
+    })
+    .then(resp => resp.json())
+    .then(weapon => {
+        let w = new Weapon(weapon.id, weapon.name, weapon.image);
     })
 }
