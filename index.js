86472cb69968d0dcf79001456c217a2d94b93f4f
Christopher Breen <C.jbreen61@gmail.com>
2021-03-12 11:27:59 -0500

    added weapons container and add html to getWeapons to pull needed data from api

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ function getWeapons(){
     fetch(BASE_URL)
     .then(resp => resp.json())
     .then(weapons => {
-        console.log(weapons);
+        weapons.forEach(weapon => {
+            const weaponMarkup = `
+            <div data-id=${weapon.id}>
+            <p>${weapon.name}</p>
+            <p>${weapon.weapon_type}<p>
+            <img src=${weapon.image}>
+            <li>${weapon.attachments.laser}</li>
+            <li>${weapon.game.name}</li>
+            </div>`;
+
+            document.querySelector('#weapons-container').innerHTML += weaponMarkup
+            
+        })
     })
 }
